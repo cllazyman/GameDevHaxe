a0657d8b891722c44d1a588ff19ff8839bd8de36
Jon Castro <castrj5@rpi.edu>
2018-09-19 14:51:18 -0400

    Making sure music works

--- source/Main.hx
+++ source/Main.hx
@@ -6,6 +6,6 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(320, 240, MenuState));
+		addChild(new FlxGame(1600, 900, MenuState));
 	}
 }
