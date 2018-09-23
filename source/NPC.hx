099d715dc2d81dfc9ebe96da8cde648a11d47ddb
Christian <leec22@rpi.edu>
2018-09-23 15:24:14 -0400

    Extra stuff

--- source/NPC.hx
+++ source/NPC.hx
@@ -11,6 +11,7 @@ class NPC extends FlxSprite {
 	// Initialize variables
 	private var nType:Int;
 	private var speed:Float = 200;
+	private var target:Player;
 
 	public function new(X:Float = 0, Y:Float = 0, NType:Int) {
 		//Set values
@@ -19,37 +20,42 @@ class NPC extends FlxSprite {
 		immovable = true;
 		
 		// Set graphics
-		loadGraphic("assets/images/enemy-" + nType + ".png", true, 16, 16);
-		setSize(8, 14);
-		offset.set(4, 2);
+		loadGraphic("assets/images/player" + nType + ".png", true, 27, 33);
+		animation.add("d", [0, 1], 6, false);
+		animation.add("l", [2, 3], 6, false);
+		animation.add("r", [4, 5], 6, false);
+		animation.add("u", [6, 7], 6, false);
+		setSize(21, 23);
+		offset.set(3, 5);
 		
 		// Set movement
 		drag.x = drag.y = 1600;
 	}
 	
 	override public function update(elapsed:Float):Void {
-		movement();
+		//follow();
 		super.update(elapsed);
 	}
 	
-	private function movement():Void {
-		/*// Set movement bools
-		var up:Bool = false;
-		var down:Bool = false;
-		var left:Bool = false;
-		var right:Bool = false;
+	public function setTarget() {
 		
-		// Get key inputs
-		up = FlxG.keys.anyPressed([UP, W]);
-		down = FlxG.keys.anyPressed([DOWN, S]);
-		left = FlxG.keys.anyPressed([LEFT, A]);
-		right = FlxG.keys.anyPressed([RIGHT, D]);
+	}
+	
+	// Moves the npc to follow the target
+	private function follow():Void {
+		/*// Get key inputs
+		var up:Bool = FlxG.keys.anyPressed([UP, W]);
+		var down:Bool = FlxG.keys.anyPressed([DOWN, S]);
+		var left:Bool = FlxG.keys.anyPressed([LEFT, A]);
+		var right:Bool = FlxG.keys.anyPressed([RIGHT, D]);
 		
 		// Cancel input if two opposite keys pressed
-		if (up && down)
+		if (up && down) {
 			up = down = false;
-		if (left && right)
+		}
+		if (left && right) {
 			left = right = false;
+		}
 		
 		// Calculate angle and velocity
 		if (up || down || left || right) {
@@ -74,15 +80,28 @@ class NPC extends FlxSprite {
 				mA = 180;
 				facing = FlxObject.LEFT;
 			} else if (right) {
-				mA = 0;
 				facing = FlxObject.RIGHT;
 			}
-
+			
 			// Determine the velocity based on angle and speed
-			velocity.set(speed, 0);
+			velocity.set(200, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), mA);
 			
+			// Change the face
+			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE) {
+				switch (facing) {
+					case FlxObject.LEFT:
+						animation.play("l");
+					case FlxObject.RIGHT:
+						animation.play("r");
+					case FlxObject.UP:
+						animation.play("u");
+					case FlxObject.DOWN:
+						animation.play("d");
+				}
+			}
 		}
+		//collisionBox.setPosition(x, y+23);
 		actionBox.setPosition(x, y);*/
 	}
 }
