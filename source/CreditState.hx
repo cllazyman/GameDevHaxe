72a803ef87bf33e7e4f29eeb9644f288833c131c
Jon Castro <castrj5@rpi.edu>
2018-09-25 20:33:41 -0400

    Added Credits

---
+++ source/CreditState.hx
@@ -0,0 +1,55 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.addons.ui.FlxButtonPlus;
+import flixel.text.FlxText;
+import flixel.util.FlxAxes;
+import flixel.util.FlxColor;
+import lime.system.System;
+
+class CreditState extends FlxState {
+	var background:FlxSprite;
+	var exitButton:FlxButtonPlus;
+	var creditText:FlxText;
+	
+	override public function create():Void {
+		// Play credits screen music
+		FlxG.sound.play(AssetPaths.credits__ogg);
+		
+		// Background
+		background = new FlxSprite(0, 0);
+		background.loadGraphic(AssetPaths.title__png, true, 640, 360);
+		background.animation.add("idle", [0, 1], 1, false);	
+		
+		//Credit Test
+		creditText = new FlxText(0, 50, 600, "Programming\nChristian Lee\nQitong Wang\n\nArt\nFanghong Dong\n\nWriting\nJimmy Jin\n\n Music\nJon Castro");
+		creditText.setFormat("assets/fonts/SHPinscher-Regular.otf", 20, FlxColor.BLUE);
+		creditText.screenCenter(FlxAxes.X);
+		creditText.wordWrap = false;
+		creditText.autoSize = false;
+		creditText.alignment = FlxTextAlign.CENTER;
+		
+		
+		// Exit Button
+		exitButton = new FlxButtonPlus(610, 10, function() {
+				System.exit(0);
+			}, "X", 19, 19);
+		exitButton.updateActiveButtonColors([FlxColor.RED]);
+		exitButton.updateInactiveButtonColors([FlxColor.BLACK]);
+		
+		// Add values to view
+		add(background);
+		add(creditText);
+		add(exitButton);
+		
+		super.create();
+	}
+	
+	// For animations
+	override public function update(elapsed:Float):Void {
+		background.animation.play("idle");
+		super.update(elapsed);
+	}
+}
